d6921985b37be6ed0587ead4f55d5d265b512351
omaralshimarey-lang <omaralshimarey@gmail.com>
2026-09-06 13:20:01 +0300

    Add extended app colors

---
+++ lib/constants/app_colors_extended.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Light Colors
+  static const Color warningLight = Color(0xFFFFF3CD);
+  static const Color successLight = Color(0xFFD4EDDA);
+  static const Color errorLight = Color(0xFFF8D7DA);
+  static const Color infoLight = Color(0xFFD1ECF1);
+}
